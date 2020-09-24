c771b8c1113b27c882da351dfc23c44eeb930a44
Александр Турчак <t-a-a-96@mail.ru>
2020-09-24 13:28:46 +0300

    Added functional testing module

---
+++ NN.Eva.Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using NN.Eva.Models;
+
+namespace NN.Eva.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceEvaNN serviceEvaNN = new ServiceEvaNN();
+
+            NetworkStructure netStructure = new NetworkStructure
+            {
+                InputVectorLength = 75,
+                NeuronsByLayers = new[] { 75, 85, 90, 1 }
+            };
+
+            TrainingConfiguration trainConfig = new TrainingConfiguration
+            {
+                StartIteration = 0,
+                EndIteration = 34000,
+                InputDatasetFilename = "TrainingSets//inputSets.txt",
+                OutputDatasetFilename = "TrainingSets//outputSets.txt",
+                MemoryFolder = "Memory"
+            };
+
+            serviceEvaNN.CreateNetwork(trainConfig.MemoryFolder, netStructure, 2);
+
+            serviceEvaNN.Train(trainConfig, 34000);
+        }
+    }
+}
