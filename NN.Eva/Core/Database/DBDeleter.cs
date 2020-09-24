07a0aa8ee912d6cf7dc800b016b3b2f1a4d890f5
Александр Турчак <t-a-a-96@mail.ru>
2020-09-24 14:39:47 +0300

    Updated database services

--- NN.Eva/Core/Database/DBDeleter.cs
+++ NN.Eva/Core/Database/DBDeleter.cs
@@ -7,41 +7,48 @@ namespace NN.Eva.Core.Database
 {
     public class DBDeleter
     {
+        /// <summary>
+        /// Name of the database for memory
+        /// </summary>
+        public string DatabaseName { get; set; }
+
         private MySqlConnection _connection;
 
         private Logger _logger;
 
-        public DBDeleter(MySqlConnection connection, Logger logger)
+        public DBDeleter(MySqlConnection connection, Logger logger, string databaseName = "memorynn")
         {
             _connection = connection;
             _logger = logger;
+
+            DatabaseName = databaseName;
         }
 
         public void DeleteFromTableNetworks(Guid id)
         {
-            DeleteFromTable(id, "memorynn", "networks", "id");
+            DeleteFromTable(id, DatabaseName, "networks", "id");
         }
 
         public void DeleteFromTableLayers(Guid networkId)
         {
-            DeleteFromTable(networkId, "memorynn", "layers", "networkId");
+            DeleteFromTable(networkId, DatabaseName, "layers", "networkId");
         }
 
         public void DeleteFromTableNeurons(Guid layerId)
         {
-            DeleteFromTable(layerId, "memorynn", "neurons", "layerId");
+            DeleteFromTable(layerId, DatabaseName, "neurons", "layerId");
         }
 
         public void DeleteFromTableWeights(Guid neuronId)
         {
-            DeleteFromTable(neuronId, "memorynn", "weights", "neuronId");
+            DeleteFromTable(neuronId, DatabaseName, "weights", "neuronId");
         }
 
         private void DeleteFromTable(Guid id, string dbName, string tableName, string rowIdName)
         {
             try
             {
-                string query = "DELETE FROM `" + dbName + "`.`" + tableName + "` WHERE (`" + rowIdName + "` = '" + id + "');";
+                string query = "DELETE FROM " + dbName + "." + tableName + " WHERE (`" + rowIdName + "` = '" + id + "');";
 
                 var command = new MySqlCommand(query, _connection);
                 command.ExecuteNonQuery();
