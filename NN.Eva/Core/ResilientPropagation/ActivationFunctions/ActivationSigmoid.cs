676033939b4c6826fc05edbc371c755f36f1a5e9
Андрей <ande5@mail.ru>
2020-10-29 00:44:55 +0300

    Added basic NeurlNetworkRProp architecture

---
+++ NN.Eva/Core/ResilientPropagation/ActivationFunctions/ActivationSigmoid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NN.Eva.Core.ResilientPropagation.ActivationFunctions
+{
+    public class ActivationSigmoid : IActivationFunction
+    {
+        public double Alpha { get; set; } = 2;
+
+        public double ActivationFunction(double x) => 1 / (1 + Math.Exp(-Alpha * x));
+
+        public double DerivativeFunction(double x)
+        {
+            double y = ActivationFunction(x);
+            return Alpha * y * (1 - y);
+        }
+
+        public double Derivative2Function(double y) => Alpha * y * (1 - y);
+    }
+}
